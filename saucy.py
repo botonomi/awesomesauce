6efca9805bf8366e19efb0561f6dc50424da0be9
lbonanomi <5369016+lbonanomi@users.noreply.github.com>
2020-04-02 13:33:58 -0400

    Update saucy.py

--- saucy.py
+++ saucy.py
@@ -1,63 +1,62 @@
-from bs4 import BeautifulSoup
-from multiprocessing import Pool
-import os
-import re
+import feedparser
 import requests
-import whisper
-import base64
-
+import string
 import sys
+import os
+import github3
 
-suspect = 'https://github.com/lbonanomi/the-book-of-secret-knowledge/blob/master/README.md'
+link = ""
+owner = os.environ['GITHUB_ACTOR']
 
-bulk = requests.get(suspect)
-soup = BeautifulSoup(bulk.text, 'html.parser')
 
-whisper_db_dir = os.getenv("HOME") + '/whispers/'
+xml = open("feed.xml", "w")
 
-#RETAINER = [(259200, 6)]                      # [(seconds_in_period, slots_in_period)]
-RETAINER = [(60, 6)]
+feed = 'https://github.com/ripienaar/free-for-dev/commits/master.atom'
+raw = feedparser.parse(feed)
 
-urls = {}
+xml.write('<?xml version="1.0" encoding="UTF-8" ?>')
+xml.write("\n")
+xml.write('<rss version="2.0">')
+xml.write('<channel>\n<title>New Free-For-Dev</title>\n<description>New Free Resources</description>\n<link>')
+xml.write(link)
+xml.write("</link>\n")
 
-for wag in soup.find_all('a', attrs={'href': re.compile("^https*://")} ):
-        link = wag.get('href').encode("utf-8")
-        encoded = base64.b64encode(link).decode().translate(str.maketrans('/', '_'))
+for entry in raw.entries:
+        xml.write("<item>\n\t<title>")
+        xml.write(entry.title)
+        xml.write("</title>\n\t<link>")
+        xml.write(entry.link)
+        xml.write('</link><description><![CDATA[')
 
-        urls[wag.get('href')] = encoded
+        patch = requests.get(entry.link + '.patch', verify=False)
 
-def whisp(item):
-        url = item[0]
-        db = item[1]
-
-        whisper_db_name = whisper_db_dir + db + '.wsp'
-
-        if not os.path.exists(whisper_db_name):
-                whisper.create(whisper_db_name, RETAINER, aggregationMethod='last')
-
-        try:
-                code = requests.get(url).status_code
-        except Exception:
-                code = 500
-
-        try:
-                whisper.update(whisper_db_name, code)
-        except Exception:
-                print("Can't update DB for ", url, "?")
-
-        (times, fails) = whisper.fetch(whisper_db_name, 315550800)
-
-        x = 0
-
-        for spot in fails:
+        for line in patch.text.split("\n"):
                 try:
-                        x = x + spot
-                except TypeError:
+                        lead = list(line)[0]
+
+                        if list(line)[0] == "+" and list(line)[1] != "+":
+                                line = line[1:]
+                                xml.write(line)
+                                xml.write("<br>\n")
+
+                except Exception:
                         continue
 
-        if x > 1200:
-                print(url + ":" + str(int(fails.pop())))
+        xml.write(' ]]></description>')
+        xml.write("\n</item>\n")
 
+xml.write("\n</channel>\n</rss>\n")
 
-p = Pool(10)
-p.map(whisp, urls.items())
+xml.close()
+
+# Connect to GitHub API and push the changes.
+github = github3.login(token=os.environ['TOKEN'])
+repository = github.repository(owner, 'BoSK-Watch')
+
+with open('feed.xml', 'rb') as fd:
+        contents = fd.read()
+
+contents_object = repository.file_contents('feed.xml')
+
+push_status = contents_object.update('automatic', contents)
+print(push_status)
